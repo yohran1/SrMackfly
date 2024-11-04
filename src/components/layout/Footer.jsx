bd512faf1aaa67f81f77e96254c936baf60c4e8b
Yohran Kallahari <yohranv@gmail.com>
2024-11-04 10:38:06 -0300

    Atualização no caminho

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer({msg}){
 
             <ul>
                 <li><a href='https://www.facebook.com/yohran.venancio/'><FaFacebookSquare /></a></li>
-                <li><a href='https://www.instagram.com/yohran_kallahari/'><FaInstagramSquare /></a></li>
+                <li><a href='https://www.instagram.com/yohranvcmorais/'><FaInstagramSquare /></a></li>
                 <li><a href='https://www.linkedin.com/in/yohran-ven%C3%A2ncio-96017b120/'><FaLinkedin /></a></li>
             </ul>
         </footer>
